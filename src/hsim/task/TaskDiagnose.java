4e719ccd822ea2063a84fabf5643bfb985207230
roboyobo <km.github@gmail.com>
2014-03-13 07:36:00 +0000

    Patients Can now be treated!

--- src/hsim/task/TaskDiagnose.java
+++ src/hsim/task/TaskDiagnose.java
@@ -12,15 +12,12 @@ public class TaskDiagnose extends Task {
 	
 	@Override
 	public void onTaskComplete() {
-		System.out.println("Yay finished diagnosing");
 		for(int i = 0; i < PlayState.mapSizeX; i++) {
 			for(int j = 0; j < PlayState.mapSizeY; j++) {
 				if(PlayState.objectTiles[i][j] != null && PlayState.objectTiles[i][j].currentTask != null) {
 					if(PlayState.objectTiles[i][j] instanceof GameObjectInstanceDoctor) {
-						if(PlayState.objectTiles[i][j].currentTask.name == "diagnose") {
-							if(PlayState.objectTiles[i][j - 1] != null && PlayState.objectTiles[i][j - 1] instanceof GameObjectInstanceBed) {
-								((GameObjectInstanceBed) PlayState.objectTiles[i][j - 1]).patientUsingBed.hasBeenDiagnosed = true;
-							}
+						if(PlayState.objectTiles[i][j - 1] != null && PlayState.objectTiles[i][j - 1] instanceof GameObjectInstanceBed) {
+							((GameObjectInstanceBed) PlayState.objectTiles[i][j - 1]).patientUsingBed.hasBeenDiagnosed = true;
 						}
 					}
 				}
--- src/hsim/task/TaskTreat.java
+++ src/hsim/task/TaskTreat.java
@@ -1,9 +1,33 @@
 package hsim.task;
 
+import java.util.Random;
+
+import hsim.object.GameObjectInstanceBed;
+import hsim.object.GameObjectInstanceDoctor;
+import hsim.state.PlayState;
+
 public class TaskTreat extends Task {
 
 	public TaskTreat() {
 		super("treat", 3, Task.DOCTOR);
 	}
-
+	
+	@Override
+	public void onTaskComplete() {
+		for(int i = 0; i < PlayState.mapSizeX; i++) {
+			for(int j = 0; j < PlayState.mapSizeY; j++) {
+				if(PlayState.objectTiles[i][j] != null && PlayState.objectTiles[i][j].currentTask != null) {
+					if(PlayState.objectTiles[i][j] instanceof GameObjectInstanceDoctor) {
+						if(PlayState.objectTiles[i][j - 1] != null && PlayState.objectTiles[i][j - 1] instanceof GameObjectInstanceBed) {
+							Random rand = new Random();
+							int num = rand.nextInt(3);
+							if(((GameObjectInstanceBed) PlayState.objectTiles[i][j - 1]).patientUsingBed.deteriorationRate - num > -5) {
+								((GameObjectInstanceBed) PlayState.objectTiles[i][j - 1]).patientUsingBed.deteriorationRate -= num;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
